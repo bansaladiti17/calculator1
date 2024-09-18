c2c78dba9e5a46d9200dd63aa6e30fe374d98f31
bansaladiti17 <aditibansal1702@gmail.com>
2024-09-18 12:18:46 +0530

    initial commit

---
+++ src/test/java/ut/CalculatorTest.java
@@ -0,0 +1,56 @@
+package ut;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.example.Calculator;
+
+public class CalculatorTest {
+
+	static Calculator cl;
+
+	@BeforeClass
+	public static void setUp() {
+		cl = new Calculator();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		System.gc();
+	}
+
+	@Test
+	public void testAdd() {
+
+		assertTrue(cl.doAdd(1, 2) == 3);
+	}
+
+	@Test
+	public void testSub() {
+		assertTrue(cl.doSub(1, 1) == 0);
+	}
+
+	@Test
+	public void testMul() {
+		assertTrue(cl.doMul(9, 2) == 18);
+	}
+
+	@Test
+	public void testDiv() {
+		assertTrue(cl.doDiv(8, 2) == 4);
+	}
+	
+	@Test
+	public void testSub1() {
+	    assertEquals(0.0, cl.doSub(2, 2),0.0);
+	}
+
+	@Test
+	public void testMul1() {
+		assertEquals(14.0,cl.doMul(7, 2),0.0);
+	}
+
+}
